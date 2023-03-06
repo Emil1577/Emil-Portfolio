f7de7606c511088c0f3daeb55db21f1f208e3f4a
Emil1577 <emilronquillo@gmail.com>
2023-03-05 20:28:51 -0800

    initial setup

---
+++ emil-portfolio/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloReact from './components/HelloReact';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+
+          Learn React
+
+      </header>
+
+      <HelloReact/>
+    </div>
+  );
+}
+
+export default App;
